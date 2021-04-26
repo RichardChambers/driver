ccbdf6d557f482eabf3a61f2f8884db1f4f0ce53
rickchambers.geo <rickchambers.geo@gmail.com>
2021-04-26 00:39:07 -0400

    Modify the font class so that we now allow the selection of either the original 16x16 bitmap font or the new 8x8 bitmap font. See Stack Overflow posting describing the change. https://stackoverflow.com/questions/67259938/reducing-memory-required-for-kedei-tft-library-used-with-3-5-tft-display-with-a

--- KeDeiTFT/KeDei_font.h
+++ KeDeiTFT/KeDei_font.h
@@ -2,12 +2,25 @@
 #define   _KeDei_FONT_H_
 #include "KeDei_TFT.h"
 
+// Following definition will allow for the use of
+// the standard sized font table or a smaller version
+// of the font table which eliminates the lower case
+// alphabetic letters which save a lot of memory.
+// This can be turned on with the Arduino Uno in order
+// to have more memory available.
+//
+// We also have found a smaller bitmap font table that
+// is about half the size of the original bitmap font table.
+// See https://github.com/dhepper/font8x8
 
-#define font_interval	1
+#define SMALLER_FONT_TABLE
+#define USE_FONT_8_B
+
 class Font{
 public:
 	void	begin(void);
 	void	set_txt(unsigned short x0,unsigned short y0,unsigned short x1,unsigned short y1,unsigned short txt_b_color);
+	void    clear_txt(void);
 	void 	clear_txt(unsigned short color);
 	void	lcd_char(char _data);
 	void	set_fontcolor(unsigned short f_color);
@@ -16,18 +29,17 @@ public:
 
 private:
 	unsigned short	font_color;
-	unsigned short	font_size;
+	unsigned short	txt_backcolor;
+	unsigned short	font_size;        // font height in pixels.
+	unsigned short  font_leading;     // interval between rows in pixels.
+	unsigned short	font_interval;    // interval between characters in pixels. kerning.
 	unsigned short	txt_x0;
 	unsigned short	txt_y0;
 	unsigned short	txt_x1;
 	unsigned short	txt_y1;
-	unsigned short	txt_backcolor;
 	unsigned short	now_x;
 	unsigned short	now_y;
-	int				char_i;
 	char			buff[16];
-	unsigned char	char_m,char_n;
-
 };
 
 extern  Font  font;
